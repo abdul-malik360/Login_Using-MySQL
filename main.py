c726e0bc49ac971b1e8eceade8c894c2a8278fc3
abdul-malik <abdulmalikmohamed360@gmail.com>
2021-06-30 13:42:39 +0200

    register complete

--- main.py
+++ main.py
@@ -29,15 +29,27 @@ def login():
             messagebox.showinfo("Login Successful", "Access Granted")
             break
 
-    if name_ent.get() != i[0] or password_ent.get() != i[1]:
+    if name_ent.get() == "" and password_ent.get() == "":
+        messagebox.showerror("No Entries", "Please Insert your name and password")
+    elif name_ent.get() != i[0] or password_ent.get() != i[1]:
         messagebox.showerror("Access Denied", "Incorrect Name or Password")
         name_ent.delete(0, END)
         password_ent.delete(0, END)
 
 
+def register():
+    messagebox.showinfo("Register", "You have been Registered")
+    my_db = mysql.connector.connect(user="abdul-malik", password="@8-2fermENt2020", host="127.0.0.1", database="mydb", auth_plugin="mysql_native_password")
+    my_cursor = my_db.cursor()
+    insert = "INSERT INTO Login (user, password) VALUES (%s, %s)"
+    entries = (name_ent.get(), password_ent.get())
+    my_cursor.execute(insert, entries)
+    my_db.commit()
+
+
 log_btn = Button(root, text="Login", command=login)
 log_btn.place(x=350, y=150)
-reg_btn = Button(root, text="Register New User")
+reg_btn = Button(root, text="Register New User", command=register)
 reg_btn.place(x=265, y=200)
 
 root.mainloop()
